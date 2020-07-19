f264896523e092948f749907b56b5ec72e3eaea4
mli <mli@student.42.fr>
2020-07-19 19:24:03 +0200

    setup.sh turns on minikube + main loop

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#!/usr/env bash
+
+############################## Turn on minikube ###############################
+
+if ! kubectl version 2>/dev/null 1>&2 ; then
+	service docker restart
+	minikube start --driver=docker
+	eval $(minikube docker-env -u)
+fi
+
 ############################## Install Metal LB ###############################
 #  Preparation
 
@@ -19,3 +30,26 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 ###############################################################################
+
+services=(		\
+	nginx		\
+#	ftps		\
+#	wordpress	\
+#	mysql		\
+#	phpmyadmin	\
+#	grafana		\
+#	influxdb	\
+)
+
+echo "Building images:"
+for service in "${services[@]}"
+do
+	echo "	Building $service image..."
+#	docker build -t $service-img ./srcs/$service >/dev/null
+#	kubectl delete -f srcs/$service-deployment.yaml >/dev/null 2>&1
+	echo "	Creating $service container..."
+#	kubectl apply -f ./srcs/$service-deployment.yaml >/dev/null
+#	while [[ $(kubectl get pods -l app=$service -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
+#		sleep 1;
+#	done
+done
