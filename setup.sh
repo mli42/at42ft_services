d606632a26019c37e707b3569c76f2270706e873
Marc LI <mli@e3r6p6.42.fr>
2020-08-12 12:28:51 +0200

    Install brew and minikube on mac + do symlinks

--- setup.sh
+++ setup.sh
@@ -15,7 +15,19 @@ if ! kubectl version 2>/dev/null 1>&2 ; then
 		sudo chown -R user42 $HOME/.kube $HOME/.minikube
 		echo "\e[91mYou'll need to do 'sudo minikube stop' when finished\e[0m"
 	else
-		minikube start --driver=virtualbox
+		##### On MacOS
+		./init_docker
+		# Install BREW 🍺
+		if ! which brew &>/dev/null; then
+			echo "\t\t\e[1;93mInstall BREW 🍺...\e[0m"
+			rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
+		fi
+		# Install minikube && kubectl
+		if ! which minikube &>/dev/null; then
+			echo "\t\t\e[1;93mInstall minikube and kubectl 🐳...\e[0m"
+			brew install minikube
+		fi
+		minikube start --driver=virtualbox || { echo "Couldn't start, try minikube delete"; exit }
 	fi
 fi
 eval $(minikube docker-env)
@@ -30,7 +42,6 @@ fi
 ############################## Install Metal LB ###############################
 
 function install_metallb () {
-	echo "Install MetalLB..."
 	#  Preparation
 
 # see what changes would be made, returns nonzero returncode if different
@@ -54,8 +65,9 @@ kubectl apply -f ./srcs/metallb-configmap.yaml
 }
 
 if ! kubectl get pods -n metallb-system 2>&1 | grep controller | grep Running >/dev/null 2>&1; then
-	install_metallb
-	kubectl apply -f dashboard-adminuser.yaml
+	echo "\t\t\e[1;93mInstall MetalLB...\e[0m"
+	install_metallb > /dev/null
+	kubectl apply -f ./srcs/dashboard-adminuser.yaml
 #	kubectl proxy &
 	kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended.yaml
 fi
